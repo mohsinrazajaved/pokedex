5d64983d03ea9419c273db348f5b2dfcd9cea579
mohsin raza <mohsinrazajaved444@gmail.com>
2017-01-25 19:45:10 +0500

    jj

--- pokedex/PokemonDetail.swift
+++ pokedex/PokemonDetail.swift
@@ -13,14 +13,14 @@ import SwiftyJSON
 class PokemonDetail
 {
     
-     private var _description: String!
-     private var _type: String!
-     private var _defense: String!
-     private var _height: String!
-     private var _weight: String!
-     private var _attack: String!
-     private var _nextEvolutionId: String!
-     private var _pokemonURL: String!
+    private var _description: String!
+    private var _type: String!
+    private var _defense: String!
+    private var _height: String!
+    private var _weight: String!
+    private var _attack: String!
+    private var _nextEvolutionId: String!
+    private var _pokemonURL: String!
     
     
      //public getters
